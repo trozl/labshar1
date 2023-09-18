9323c1d328732509cf4b0945ff3b1bfac8e69f25
Trozl <irakitinn91@gmail.com>
2023-09-18 12:45:36 +0300

    Добавьте файлы проекта.

---
+++ labshar1/FirstNum.cs
@@ -0,0 +1,25 @@
+﻿class FirstNum
+{
+    private double x, y, z;
+    private double a, b;
+    public void InputXYZ()
+    {
+        Console.WriteLine("Введите x:");
+        x = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите y:");
+        y = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите z:");
+        z = Convert.ToDouble(Console.ReadLine());
+    }
+    public void Calculate()
+    {
+        a = (3 + Math.Pow(Math.E, 2) / (1 + Math.Pow(x, 2) * Math.Abs(y - Math.Tan(z))));
+        b = 1+Math.Abs(y-x)+((Math.Pow((y-x),2))/2)+((Math.Pow((x-y),2))/3);
+    }
+    public void ShowCalc()
+    {
+        Console.WriteLine($"При x = {x} y = {y} z = {z}");
+        Console.WriteLine($"a = {a} и b = {b}");
+    }
+
+};
---
+++ labshar1/Program.cs
@@ -0,0 +1,5 @@
+﻿var obj1 = new FirstNum();
+obj1.InputXYZ();
+obj1.Calculate();
+obj1.ShowCalc();
+

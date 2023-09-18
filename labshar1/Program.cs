a58d7ade675ab121ae05085a6ca45686575f4a90
Trozl <irakitinn91@gmail.com>
2023-09-18 13:21:49 +0300

    num2

--- labshar1/Program.cs
+++ labshar1/Program.cs
@@ -1,5 +1,37 @@
-﻿var obj1 = new FirstNum();
-obj1.InputXYZ();
-obj1.Calculate();
-obj1.ShowCalc();
+﻿using labshar1;
+using System.Security.Cryptography.X509Certificates;
 
+int i = 1;
+while (i == 1)
+{
+    Console.WriteLine("Введите номер задания для выполнения\n1. Вычисление по формулам\n2. Вычисление радиуса вписанной окружности треугольника по координатам его вершин\n3. Вычисление модуля\n4. Времена года\n5. Выход из программы");
+    int num = Convert.ToInt32(Console.ReadLine());
+    switch (num){
+    case 1:
+        Console.WriteLine("Начало первого задания");
+        var obj1 = new FirstNum();
+        obj1.InputXYZ();
+        obj1.Calculate();
+        obj1.ShowCalc();
+        Console.WriteLine("Конец первого задания");
+    break;
+    case 2:
+        Console.WriteLine("Начало второго задания");
+        var obj2 = new SecondNum();
+        obj2.InputTrianglePoints();
+        obj2.CalculateInnerCircleRadius();
+        obj2.ShowCalculate();
+        Console.WriteLine("Конец второго задания");
+    break;
+        case 3:
+            Console.WriteLine("W.I.P");
+            break;
+        case 4:
+            Console.WriteLine("W.I.P");
+            break;
+        case 5:
+            Console.WriteLine("Завершение работы программы.\n Досивдания");
+            i = 0;
+            break;
+}
+}
---
+++ labshar1/SecondNum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labshar1
+{
+    class SecondNum
+    {
+        private double x1, x2, y1, y2, x3, y3;
+        private double s, r;
+        private double line1, line2, line3;
+        public void InputTrianglePoints()
+        {
+            Console.WriteLine("Введите коорданаты 1 точки:");
+            x1 = Convert.ToDouble(Console.ReadLine());
+            y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите коорданаты 2 точки:");
+            x2 = Convert.ToDouble(Console.ReadLine());
+            y2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите коорданаты 3 точки:");
+            x3 = Convert.ToDouble(Console.ReadLine());
+            y3 = Convert.ToDouble(Console.ReadLine());
+
+        }
+        public void CalculateInnerCircleRadius()
+        {
+            s = 1 / 2 * Math.Abs((x2 - x1) * (y3 - y1) - (x3 - x1)*(y2 - y1));
+            line1 = Math.Sqrt((Math.Pow((x1 - x2), 2)) + Math.Pow((y1 - y2), 2));
+            line2 = Math.Sqrt((Math.Pow((x3 - x2), 2)) + Math.Pow((y3 - y2), 2));
+            line3 = Math.Sqrt((Math.Pow((x1 - x3), 2)) + Math.Pow((y1 - y3), 2));
+            r = (2 * s) / (line1 + line2 + line3);
+        }
+        public void ShowCalculate()
+        {
+            Console.WriteLine($"При коорданатах\nx1 = {x1} y1 = {y1}\n x2 = {x2} y2 = {y2}\n x3 = {x3} y3 = {y3}");
+            Console.WriteLine($"Радиус вписанной окружности (r) = {r}");
+            Console.WriteLine($"Площадь треугольника (s) = {s}");
+        }
+    }
+}
